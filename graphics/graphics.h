4534870cdf016a60a9aafaf16bb49fd65e682acb
Ryan <rwspelde15@gmail.com>
2026-03-19 20:21:14 -0500

    updated graphics to support sprites

--- graphics/graphics.h
+++ graphics/graphics.h
@@ -1,28 +1,34 @@
 #pragma once
-#include <string>
 
-#include "SDL3/SDL_render.h"
-#include "SDL3/SDL_video.h"
+#include <SDL3/SDL.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "sprite.h"
 
 class Color {
 public:
-    int red {255}, green {0}, blue {0}, alpha {255};
-
+    int red{255}, green{0}, blue{255}, alpha{255};
 };
 
 class Graphics {
 public:
-    Graphics(const std::string& title, int window_width, int window_height);
-    void clear() const;
+    Graphics(std::string title, int width, int height);
     void draw(const SDL_FRect& rect, const Color& color, bool filled=true);
+    void clear();
     void update();
+    void set_title(const std::string& title);
 
-    const int width, height;
+    int get_texture_id(const std::string& image_filename);
+    void draw_sprite(const Vec<float>& pixel, const Sprite& sprite);
+
+    const int width;
+    const int height;
 
 private:
-    std::string title;
-
     SDL_Window* window;
     SDL_Renderer* renderer;
-
+    std::vector<SDL_Texture*> textures;
+    std::pmr::unordered_map<std::string, int> texture_ids;
 };
